68d782170bfb072bfbe2253992dd90b7ac58c985
Julien Plée <julplee@gmail.com>
2014-02-19 20:34:47 +0100

    Add first failing test

---
+++ Bowling/Bowling/Scorer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+    class Scorer
+    {
+        internal void Throw(int p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Score { get; set; }
+    }
+}
---
+++ Bowling/Bowling/ScorerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Bowling
+{
+    [TestFixture]
+    public class ScorerTests
+    {
+        [Test]
+        public void Scorer_ThrowFivePins_ScoreEqualFive()
+        {
+            Scorer scorer = new Scorer();
+
+            scorer.Throw(5);
+
+            Assert.AreEqual(5, scorer.Score);
+        }
+    }
+}
